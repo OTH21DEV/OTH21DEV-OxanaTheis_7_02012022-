a53fb6acf24bf48e85abd065155e5d4df67bb3fe
Oxana Theis <otheis@protonmail.com>
2022-04-14 23:25:11 +0200

    version avec correction getWords

--- scripts/utils/getWords.js
+++ scripts/utils/getWords.js
@@ -33,36 +33,28 @@ function getWords(element) {
 }
 */
 function getWords(element) {
-  let testWords = [];
-
-  let recipeWords = [];
-  let test = [];
   let words = [];
+  let recipeWords = [];
   let listeIngredients = getIngredients(element);
 
-  //concatenation des mots
-  words = [...new Set(words.concat(element.name.toLocaleLowerCase(), listeIngredients, element.description.toLocaleLowerCase(), element.appliance.toLocaleLowerCase(), element.ustensils))].join(" ");
-  //split en sous chaine
-  let arrayWords = words.split(" ");
-
-  //supression de ponctuation
-  for (let word of arrayWords) {
-    word = word.replace(/[,;.]$/, "");
-    test.push(word);
-  }
-
-  arrayWords = test;
+  //concatenation des mots, supression de ponctuation,split en sous chaine
+  words = [...new Set(words.concat(element.name.toLocaleLowerCase(), listeIngredients, element.description.toLocaleLowerCase(), element.appliance.toLocaleLowerCase(), element.ustensils))]
+    .join(" ")
+    .replace(/[,;.]$/, "")
+    .split(" ");
 
   //retour de mots contenant plus que 2 lettres
-  let filteredWordsByLength = arrayWords.filter(function (word) {
+
+  words = words.filter(function (word) {
     return word.length > 2;
   });
 
   //supression de doublon
-  recipeWords = [...new Set(recipeWords.concat(filteredWordsByLength))].sort();
 
-  testWords = { id: element.id, mots: recipeWords };
+  recipeWords = [...new Set(recipeWords.concat(words))].sort();
 
-  return testWords;
+  recipeWords = { id: element.id, mots: recipeWords };
+
+  return recipeWords;
 }
 export { getWords };
